2fa563a29738612e2c709722e9f3ed84198c13bf
xiao233333 <xiaotan0307@gmail.com>
2024-09-11 17:00:34 +1000

    skimage import

--- histocartography/preprocessing/superpixel.py
+++ histocartography/preprocessing/superpixel.py
@@ -11,7 +11,7 @@ import cv2
 import h5py
 import numpy as np
 from skimage.color.colorconv import rgb2hed
-from skimage.future import graph
+from skimage import graph
 from skimage.segmentation import slic
 
 from ..pipeline import PipelineStep
